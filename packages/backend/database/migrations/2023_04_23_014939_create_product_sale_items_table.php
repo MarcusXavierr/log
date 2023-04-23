8ffbd3f3ebc42ec582c2a6f47d28f6fed880303b
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-23 15:12:48 -0300

    feat(database): create migration to add product_sale_items table to database schema

---
+++ packages/backend/database/migrations/2023_04_23_014939_create_product_sale_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_sale_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sale_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('price_per_unit');
+            $table->unsignedBigInteger('total_revenue');
+            $table->unsignedBigInteger('units_sold');
+
+            $table->foreign('sale_id')->references('id')->on('sales');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_sale_items');
+    }
+};
