b4a9f1ca7b342c918cf132d971993fad750b1056
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-23 15:04:17 -0300

    feat(CreateRandomProductSaleData.php): add CreateRandomProductSaleData service class with methods to create random product sale data.

---
+++ packages/backend/app/Services/CreateRandomProductSaleData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use App\DataTransferObjects\ProductSaleItemData;
+use Illuminate\Support\Collection;
+
+class CreateRandomProductSaleData
+{
+    public function createRandomItemData(Product $product, int $maxUnits = 10): ProductSaleItemData
+    {
+        $randomNumberOfUnits = rand(1, $maxUnits);
+
+        $data = [
+            'units' => $randomNumberOfUnits,
+            'pricePerUnit' => $product->price,
+            'totalPrice' => $product->price * $randomNumberOfUnits,
+        ];
+
+        return new ProductSaleItemData(...$data);
+    }
+
+    public function createRandomCollection(Collection $prices, $maxUnitsPerProduct = 10): Collection
+    {
+        return $prices->map(function ($product) use ($maxUnitsPerProduct) {
+            return $this->createRandomItemData($product, $maxUnitsPerProduct);
+        });
+    }
+}
