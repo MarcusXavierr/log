0538d424cbb4ce585a62e7412d6f39fd602a35d1
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-23 15:12:30 -0300

    feat(Product.php): add sales and productSaleItem relationships to Product model

--- packages/backend/app/Models/Product.php
+++ packages/backend/app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -13,4 +15,15 @@ class Product extends Model
         'name',
         'price'
     ];
+
+    //TODO test
+    public function sales(): BelongsToMany
+    {
+        return $this->belongsToMany(ProductSaleItem::class, 'product_sale_items');
+    }
+
+    public function productSaleItem(): HasMany
+    {
+        return $this->hasMany(ProductSaleItem::class);
+    }
 }
