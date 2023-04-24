48e0e3f435f2c5cc83578e54f5d5302ea19b2eed
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-24 15:19:22 -0300

    feat: Add data filtering functionality to DashboardController's index method

--- packages/backend/app/Http/Controllers/DashboardController.php
+++ packages/backend/app/Http/Controllers/DashboardController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTransferObjects\FilterOptionsData;
 use App\Http\Requests\DataFilterRequest;
-use Illuminate\Http\Request;
+use App\Services\Dashboard\SalesDashboardService;
+use App\Services\Dashboard\SoldProductsService;
+use Exception;
 
 class DashboardController extends Controller
 {
     public function index(DataFilterRequest $request)
     {
-        dd($request->all(), $request->validated());
+        try {
+            $data = $request->validated();
+            $filter = new FilterOptionsData($data);
+
+            return response()->json([
+                'sales_data' => (new SalesDashboardService($filter))->getData(),
+                'products_data' => (new SoldProductsService($filter))->getData()
+            ]);
+        } catch(Exception $e) {
+            report($e);
+            abort(500, 'Could not get data, try again later');
+        }
     }
 }
