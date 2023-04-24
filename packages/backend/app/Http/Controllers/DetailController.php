c6fef634d6bb3c61d0854e27720e6299d754d33d
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-24 15:47:51 -0300

    feat: implement controllers to return paginated data on details page

--- packages/backend/app/Http/Controllers/DetailController.php
+++ packages/backend/app/Http/Controllers/DetailController.php
@@ -2,18 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTransferObjects\FilterOptionsData;
 use App\Http\Requests\DataFilterRequest;
-use Illuminate\Http\Request;
+use App\Services\Dashboard\SalesDashboardService;
+use App\Services\Dashboard\SoldProductsService;
+use Exception;
 
 class DetailController extends Controller
 {
     public function detailProducts(DataFilterRequest $request)
     {
+        try {
+            $data = $request->validated();
+            $filter = new FilterOptionsData($data);
 
+            return response()->json([
+                'data' => (new SoldProductsService($filter))->getPaginatedData()
+            ]);
+        } catch (Exception $e) {
+            report($e);
+            abort(500, 'Could not get detailment of products, try again later');
+        }
     }
 
     public function detailSales(DataFilterRequest $request)
     {
+        try {
+            $data = $request->validated();
+            $filter = new FilterOptionsData($data);
 
+            return response()->json([
+                'data' => (new SalesDashboardService($filter))->getPaginatedData()
+            ]);
+        } catch (Exception $e) {
+            report($e);
+            abort(500, 'Could not get detailment of products, try again later');
+        }
     }
 }
