6fe7a937aac5d551145ade739be965fd60ae776a
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-24 00:13:08 -0300

    feat: create routes

--- packages/backend/routes/web.php
+++ packages/backend/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DetailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/detail-sales', [DetailController::class, 'detailSales']);
+Route::get('/detail-products', [DetailController::class, 'detailProducts']);
