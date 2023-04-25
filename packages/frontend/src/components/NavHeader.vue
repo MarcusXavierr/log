0d70a372613ca61271d0ca92d0900b92223e5451
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-25 18:25:38 -0300

    Add NavHeader component with logo to the frontend package

---
+++ packages/frontend/src/components/NavHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="bg-primary">
+    <img :src="logo" alt="a beautiful logo"/>
+  </nav>
+</template>
+
+<script lang="ts">
+import logo from '../assets/logo.svg'
+
+export default {
+  name: 'NavHeader',
+  data() {
+    return {
+      logo
+    }
+  }
+}
+</script>
+
+<style scoped>
+nav {
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
