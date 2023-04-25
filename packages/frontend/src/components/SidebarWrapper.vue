89394a41ec9719f8302b733224e6957c81b738a9
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-25 18:25:18 -0300

    feat(SidebarWrapper.vue): add SidebarWrapper component with FilterSidebar component and getDashboardData method from Vuex store

---
+++ packages/frontend/src/components/SidebarWrapper.vue
@@ -0,0 +1,27 @@
+<template>
+  <aside class="drawer drawer-mobile" data-theme="light">
+    <input id="sidebar" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content p-4">
+      <label for="sidebar" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+      <slot>Void</slot>
+    </div>
+    <FilterSidebar />
+  </aside>
+</template>
+
+<script lang="ts">
+import FilterSidebar from '@/components/FilterSidebar.vue'
+import { mapActions } from 'vuex'
+export default {
+  name: 'SidebarWrapper',
+  components: {
+    FilterSidebar
+  },
+  methods: {
+    ...mapActions(['getDashboardData'])
+  },
+  mounted() {
+    this.getDashboardData()
+  }
+}
+</script>
