97dacc9a20ac5f120adec12213a278e74c404e2b
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-25 23:29:59 -0300

    feat: Add DetailPage.vue component with a table that displays product details and a button to go back to the dashboard, and fetches and populates the table with product data based on the filter state

---
+++ packages/frontend/src/views/DetailPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <h2 class="text-4xl text-center">Página de detalhamento dos produtos</h2>
+  <button class="btn btn-primary" @click="goToDashboard()">Voltar para o dashboard</button>
+  <div class="overflow-x-auto">
+    <table class="table w-full">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Unidades vendidas</th>
+          <th>Total Vendido</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in productsData" :key="product.id">
+          <td>{{ product.name }}</td>
+          <td>R${{ product.price / 100 }}</td>
+          <td>{{ product.units_sold }}</td>
+          <td>R${{ product.total_revenue }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { mapState, mapActions, mapMutations } from 'vuex'
+export default {
+  name: 'DetailPage',
+  data() {
+    return {
+      productsData: []
+    }
+  },
+  computed: {
+    ...mapState(['filter'])
+  },
+  methods: {
+    ...mapActions(['getProductsDetails']),
+    ...mapMutations(['goToDashboard']),
+    populateTable({ data }) {
+      this.productsData = data.data
+    }
+  },
+  watch: {
+    filter: {
+      handler() {
+        this.getProductsDetails().then(this.populateTable)
+      },
+      deep: true,
+      immediate: true
+    }
+  }
+}
+</script>
