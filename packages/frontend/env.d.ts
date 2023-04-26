9791e7deb6e07aa6678a7477d41840ed900179fa
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-25 23:27:03 -0300

    chore: add new type on front

--- packages/frontend/env.d.ts
+++ packages/frontend/env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 interface State {
+  filter: Filter
+  isOnDetailPage: boolean
+}
+
+interface Filter {
   minPrice?: number
   maxPrice?: number
   startDate?: string
